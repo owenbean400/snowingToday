29fe1936b598ddc76a3952193a31bfbaf402b6f0
owenbean400 <owenbean400@gmail.com>
2023-01-02 13:23:13 -0500

    Added API request

--- main.js
+++ main.js
@@ -1,164 +1,41 @@
 const { createApp } = Vue;
 
-const weatherData = [
-  {
-    postalCode: "04101",
-    country: "United States",
-    district: "Maine",
-    city: "Portland",
-    latitude: 43.661,
-    longitude: -70.255,
-    snowDayProbability: 0.45,
-    snowData: {
-      totalSnow: 0.1,
-      snowingHours: 0.1,
-      prediction: "No chance of school being cancelled from snow",
-    },
-  },
-  {
-    postalCode: "13790",
-    country: "United States",
-    district: "New York",
-    city: "Maine",
-    latitude: 42.193,
-    longitude: -76.061,
-    snowDayProbability: 0.86,
-    snowData: {
-      totalSnow: 1.8,
-      snowingHours: 1.0,
-      prediction: "Slight possible delay of school",
-    },
-  },
-  {
-    postalCode: "97204",
-    country: "United States",
-    district: "Oregon",
-    city: "Portland",
-    latitude: 45.52,
-    longitude: -122.674,
-    snowDayProbability: 0.0,
-    snowData: {
-      totalSnow: 0.0,
-      snowingHours: 0.0,
-      prediction: "No chance of school being cancelled from snow",
-    },
-  },
-  {
-    postalCode: "78374",
-    country: "United States",
-    district: "Texas",
-    city: "Portland",
-    latitude: 27.877,
-    longitude: -97.323,
-    snowDayProbability: 0.0,
-    snowData: {
-      totalSnow: 0.0,
-      snowingHours: 0.0,
-      prediction: "No chance of school being cancelled from snow",
-    },
-  },
-  {
-    postalCode: "37148",
-    country: "United States",
-    district: "Tennessee",
-    city: "Portland",
-    latitude: 36.582,
-    longitude: -86.516,
-    snowDayProbability: 0.0,
-    snowData: {
-      totalSnow: 0.0,
-      snowingHours: 0.0,
-      prediction: "No chance of school being cancelled from snow",
-    },
-  },
-  {
-    postalCode: "45039",
-    country: "United States",
-    district: "Ohio",
-    city: "Maineville",
-    latitude: 39.315,
-    longitude: -84.221,
-    snowDayProbability: 0.0,
-    snowData: {
-      totalSnow: 0.0,
-      snowingHours: 0.0,
-      prediction: "No chance of school being cancelled from snow",
-    },
-  },
-  {
-    postalCode: "48875",
-    country: "United States",
-    district: "Michigan",
-    city: "Portland",
-    latitude: 42.869,
-    longitude: -84.903,
-    snowDayProbability: 0.0,
-    snowData: {
-      totalSnow: 0.0,
-      snowingHours: 0.0,
-      prediction: "No chance of school being cancelled from snow",
-    },
-  },
-  {
-    postalCode: "47371",
-    country: "United States",
-    district: "Indiana",
-    city: "Portland",
-    latitude: 40.434,
-    longitude: -84.978,
-    snowDayProbability: 0.0,
-    snowData: {
-      totalSnow: 0.0,
-      snowingHours: 0.0,
-      prediction: "No chance of school being cancelled from snow",
-    },
-  },
-  {
-    postalCode: "06459",
-    country: "United States",
-    district: "Connecticut",
-    city: "Portland",
-    latitude: 41.573,
-    longitude: -72.641,
-    snowDayProbability: 0.0,
-    snowData: {
-      totalSnow: 0.0,
-      snowingHours: 0.0,
-      prediction: "No chance of school being cancelled from snow",
-    },
-  },
-  {
-    postalCode: "71663",
-    country: "United States",
-    district: "Arkansas",
-    city: "Portland",
-    latitude: 33.238,
-    longitude: -91.512,
-    snowDayProbability: 0.0,
-    snowData: {
-      totalSnow: 0.0,
-      snowingHours: 0.0,
-      prediction: "No chance of school being cancelled from snow",
-    },
-  },
-];
-
 createApp({
   methods: {
-    search() {
-      let data = weatherData;
+    async search() {
+      this.loading = true;
 
-      if (!Array.isArray(data)) {
-        errorMsg = "Error Searching";
-        return;
-      } else if (data.length == 0) {
-        errorMsg = "No places found";
-        return;
-      } else if (data.length == 1) {
-        // Redirect
-      } else {
-        this.searchOptions = data;
+      try {
+        const response = await axios.get(
+          "http://localhost:8080/api/v1/locations",
+          {
+            params: {
+              query: this.querySearch,
+            },
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+
+        let data = response.data.data;
+        if (!Array.isArray(data)) {
+          this.errorMsg = "Error searching!";
+        } else if (data.length == 0) {
+          this.errorMsg = "No places found!";
+        } else if (data.length == 1) {
+          this.searchOptions = data;
+          this.selected(data[0]);
+          this.errorMsg = "";
+        } else {
+          this.searchOptions = data;
+          this.errorMsg = "";
+        }
+      } catch (error) {
+        this.errorMsg = "Error searching from server!";
       }
+
+      this.loading = false;
       this.date = new Date();
     },
     selected(result) {
@@ -196,6 +73,11 @@ createApp({
           return "Dec";
       }
     },
+    getHours(date) {
+      if (date.getHours() == 0) return 12;
+      if (date.getHours() == 12) return 12;
+      return date.getHours() % 12;
+    },
     shouldShowColor(index) {
       return this.snowData.snowDayProbability * 90 > index;
     },
@@ -207,6 +89,8 @@ createApp({
       display: 0,
       snowData: {},
       date: new Date(),
+      loading: false,
+      querySearch: "",
     };
   },
 }).mount("#app");
